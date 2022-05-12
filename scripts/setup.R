b003db36b874529ad8412acbcead03dda265cf16
Elodie <79649046+Elodie-kwan@users.noreply.github.com>
2022-05-12 21:33:33 +0200

    attempt variable importance logreg

--- scripts/setup.R
+++ scripts/setup.R
@@ -7,7 +7,17 @@ packages <- c(
 "tidyverse", "readxl", "Hmisc", "DataExplorer", 
 "psych", "rpart", "gridExtra", "summarytools",
 "dplyr",  "ggplot2", "factoextra", "rpart.plot",
-"adabag", "caret","e1071"
+"adabag", "caret","e1071", 
+"DALEX", "vip"
 )
 
 purrr::walk(packages, library, character.only = TRUE)
+
+# function that calculates variable importance 
+calculate_importance <- function(your_model_explainer, n_permutations = 10){
+  imp <- model_parts(explainer = your_model_explainer,
+                     B = n_permutations,
+                     type = "ratio",
+                     N = NULL)
+  return(imp)
+}
